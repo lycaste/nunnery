08f2d8e2f6cde793021711aee10fa4b299d53fb2
yaohao <yaoha403@126.como>
2016-02-04 22:24:26 +0800

    Initial Commit

---
+++ nunnery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nunnery
+//
+//  Created by yh on 2/4/16.
+//  Copyright © 2016 yh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
